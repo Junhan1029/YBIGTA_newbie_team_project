7a10dfcc27ddce1904b774b653c817f96c6078ac
Junhan Chang <highnd29@gmail.com>
2026-01-13 14:40:33 +0900

    Upload team project_service.py file done

---
+++ app/user/user_service.py
@@ -0,0 +1,42 @@
+from app.user.user_repository import UserRepository
+from app.user.user_schema import User, UserLogin, UserUpdate
+
+class UserService:
+    def __init__(self, userRepoitory: UserRepository) -> None:
+        self.repo = userRepoitory
+
+    def login(self, user_login: UserLogin) -> User:
+        ## TODO
+        user = self.repo.get_user_by_email(user_login.email)
+        if user is None:
+            raise ValueError("User not Found.")
+
+        if user.password != user_login.password:
+            raise ValueError("Invalid ID/PW")
+
+        return user
+        
+    def register_user(self, new_user: User) -> User:
+        ## TODO
+        existing_user = self.repo.get_user_by_email(new_user.email)
+        if existing_user is not None:
+            raise ValueError("User already Exists.")
+
+        return self.repo.save_user(new_user)
+
+    def delete_user(self, email: str) -> User:
+        ## TODO
+        user = self.repo.get_user_by_email(email)
+        if user is None:
+            raise ValueError("User not Found.")
+
+        return self.repo.delete_user(user)
+
+    def update_user_pwd(self, user_update: UserUpdate) -> User:
+        ## TODO
+        user = self.repo.get_user_by_email(user_update.email)
+        if user is None:
+            raise ValueError("User not Found.")
+
+        user.password = user_update.new_password
+        return self.repo.save_user(user)
