422d112e053a598710f54d712f2b20569d9b49d0
Junhan Chang <highnd29@gmail.com>
2026-01-14 13:17:09 +0900

    user_router.py file done.

--- app/user/user_router.py
+++ app/user/user_router.py
@@ -9,6 +9,7 @@ user = APIRouter(prefix="/api/user")
 
 @user.post("/login", response_model=BaseResponse[User], status_code=status.HTTP_200_OK)
 def login_user(user_login: UserLogin, service: UserService = Depends(get_user_service)) -> BaseResponse[User]:
+    
     try:
         user = service.login(user_login)
         return BaseResponse(status="success", data=user, message="Login Success.") 
@@ -19,16 +20,30 @@ def login_user(user_login: UserLogin, service: UserService = Depends(get_user_se
 @user.post("/register", response_model=BaseResponse[User], status_code=status.HTTP_201_CREATED)
 def register_user(user: User, service: UserService = Depends(get_user_service)) -> BaseResponse[User]:
     ## TODO
-    return None
+    try:
+        new_user: User = service.register_user(user)
+        return BaseResponse(status="success", data=new_user, message="User registeration success.")
+    except ValueError as e:
+        raise HTTPException(status_code=400, detail=str(e))
+    
 
 
 @user.delete("/delete", response_model=BaseResponse[User], status_code=status.HTTP_200_OK)
 def delete_user(user_delete_request: UserDeleteRequest, service: UserService = Depends(get_user_service)) -> BaseResponse[User]:
     ## TODO
-    return None
+    try:
+        deleted_user: User = service.delete_user(str(user_delete_request.email))
+        return BaseResponse(status="success", data=deleted_user, message="User Deletion Success.")
+    except ValueError as e:
+        raise HTTPException(status_code=404, detail=str(e))
 
 
 @user.put("/update-password", response_model=BaseResponse[User], status_code=status.HTTP_200_OK)
 def update_user_password(user_update: UserUpdate, service: UserService = Depends(get_user_service)) -> BaseResponse[User]:
     ## TODO
-    return None
+    try:
+        updated_user: User = service.update_user_pwd(user_update)
+        return BaseResponse(status="success", data=updated_user, message="User password update success.")
+    except ValueError as e:
+        raise HTTPException(status_code=404, detail=str(e))
+    
