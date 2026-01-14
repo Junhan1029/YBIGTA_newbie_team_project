70e84b14ff79741b413a98c3979bdc3f56a22044
iiihyeong <iiihyeong@yonsei.ac.kr>
2026-01-14 14:08:10 +0900

    feat: user_router 및 user_service 구현 완료

--- app/user/user_router.py
+++ app/user/user_router.py
@@ -9,7 +9,14 @@ user = APIRouter(prefix="/api/user")
 
 @user.post("/login", response_model=BaseResponse[User], status_code=status.HTTP_200_OK)
 def login_user(user_login: UserLogin, service: UserService = Depends(get_user_service)) -> BaseResponse[User]:
-    
+    '''
+    User가 로그인을 시도하면 해당 데이터를 user_login 인스턴스에 저장합니다
+    이후 FastAPI가 get_user_service를 실행시킵니다
+
+    Args:
+        user_login: 로그인 정보(이메일, 비밀번호)
+        service: get_user_service로 생성된 작업 객체가 담긴 변수
+    '''
     try:
         user = service.login(user_login)
         return BaseResponse(status="success", data=user, message="Login Success.") 
@@ -19,7 +26,10 @@ def login_user(user_login: UserLogin, service: UserService = Depends(get_user_se
 
 @user.post("/register", response_model=BaseResponse[User], status_code=status.HTTP_201_CREATED)
 def register_user(user: User, service: UserService = Depends(get_user_service)) -> BaseResponse[User]:
-    ## TODO
+    '''
+    받은 회원가입 user 정보를 database에 등록합니다
+    이미 존재하는 user 정보일 시, 400 에러로 핸들링하여 처리합니다.
+    '''
     try:
         new_user: User = service.register_user(user)
         return BaseResponse(status="success", data=new_user, message="User registeration success.")
@@ -30,7 +40,10 @@ def register_user(user: User, service: UserService = Depends(get_user_service)) 
 
 @user.delete("/delete", response_model=BaseResponse[User], status_code=status.HTTP_200_OK)
 def delete_user(user_delete_request: UserDeleteRequest, service: UserService = Depends(get_user_service)) -> BaseResponse[User]:
-    ## TODO
+    '''
+    받은 삭제 요청 user_delete_request 정보를 database에서 삭제합니다
+    database에 존재하지 않는 user_delete_request 정보일 시, 404 에러로 핸들링하여 처리합니다.
+    '''
     try:
         deleted_user: User = service.delete_user(str(user_delete_request.email))
         return BaseResponse(status="success", data=deleted_user, message="User Deletion Success.")
@@ -40,7 +53,10 @@ def delete_user(user_delete_request: UserDeleteRequest, service: UserService = D
 
 @user.put("/update-password", response_model=BaseResponse[User], status_code=status.HTTP_200_OK)
 def update_user_password(user_update: UserUpdate, service: UserService = Depends(get_user_service)) -> BaseResponse[User]:
-    ## TODO
+    '''
+    받은 비밀번호 변경 요청 user_update 정보를 통해 database에 있는 원본 비밀번호를 업데이트합니다
+    원본 user_update 정보가 database에 존재하지 않을 시, 404 에러로 핸들링하여 처리합니다.
+    '''
     try:
         updated_user: User = service.update_user_pwd(user_update)
         return BaseResponse(status="success", data=updated_user, message="User password update success.")
--- app/user/user_service.py
+++ app/user/user_service.py
@@ -6,7 +6,15 @@ class UserService:
         self.repo = userRepoitory
 
     def login(self, user_login: UserLogin) -> User:
-        ## TODO
+        '''
+        유저 로그인 정보 user_login을 파라미터로 받아서,
+        비밀번호가 같다면 user 접속을 허용하고 
+        같지 않다면 ValueError exception을 핸들링하여
+        접근을 차단합니다
+
+        유저 로그인 정보 자체가 database에 없다면 "User not Found."를 출력하고 
+        exception 핸들링 처리합니다
+        '''
         user = self.repo.get_user_by_email(user_login.email)
         if user is None:
             raise ValueError("User not Found.")
@@ -17,7 +25,11 @@ class UserService:
         return user
         
     def register_user(self, new_user: User) -> User:
-        ## TODO
+        '''
+        새 유저 정보 new_user를 받아서 database에 등록합니다.
+
+        해당 정보가 이미 database에 있다면 처리를 차단하고 exception 핸들링 합니다.
+        '''
         existing_user = self.repo.get_user_by_email(new_user.email)
         if existing_user is not None:
             raise ValueError("User already Exists.")
@@ -25,7 +37,11 @@ class UserService:
         return self.repo.save_user(new_user)
 
     def delete_user(self, email: str) -> User:
-        ## TODO
+        '''
+        삭제 유저 정보 email를 받아서 database에서 해당 이메일을 가진 유저 정보를 삭제합니다. 
+
+        해당 정보가 database에 없다면 처리를 차단하고 exception 핸들링 합니다.
+        '''
         user = self.repo.get_user_by_email(email)
         if user is None:
             raise ValueError("User not Found.")
@@ -33,7 +49,12 @@ class UserService:
         return self.repo.delete_user(user)
 
     def update_user_pwd(self, user_update: UserUpdate) -> User:
-        ## TODO
+        '''
+        비밀번호 업데이트 유저 정보 user_update를 받아서, 
+        database에서 해당 유저의 비밀번호를 업데이트합니다. 
+
+        해당 정보가 database에 없다면 처리를 차단하고 exception 핸들링 합니다.
+        '''
         user = self.repo.get_user_by_email(user_update.email)
         if user is None:
             raise ValueError("User not Found.")
